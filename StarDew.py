45eeabbbde21814e2d6671acecf11e4a49fa4fe4
Ak627 <91678317+Ak627@users.noreply.github.com>
2022-10-26 07:44:20 -0600

    Create StarDew.py

---
+++ StarDew.py
@@ -0,0 +1,26 @@
+import pygame
+import sys
+from settings import *
+from level import Level#import the level file
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.level = Level()
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                    
+                dt = self.clock.tick()/1000#fps
+                self.level.run(dt)
+                pygame.display.update()
+                
+if __name__=='__main__':
+    game = Game()
+    game.run()
